44237b972dda8c2e5e57b3ddc43d960351907e75
theark00 <143406995+theark00@users.noreply.github.com>
2024-05-17 18:38:56 +0200

    Add files via upload

---
+++ ViewModels/CourseIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace WebApp.ViewModels;
+
+public class CourseIndexViewModel
+{
+    public IEnumerable<CourseViewModel> Courses { get; set; } = [];
+}
---
+++ ViewModels/SubscribeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels;
+
+public class SubscribeViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "E-mail", Prompt = "Your Email")]
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekinReview {  get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+}
