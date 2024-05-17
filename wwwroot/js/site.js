92064578eb0663ee5eaf9794824e64cfce31fa3e
theark00 <143406995+theark00@users.noreply.github.com>
2024-05-17 18:57:44 +0200

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    handleProfileImageUpload()
+})
+
+
+function handleProfileImageUpload() {
+    try {
+        let fileUploader = document.querySelector('#fileUploader')
+        if (fileUploader != undifined) {
+            fileUploader.addEventListener('change', function () {
+                if (this.files.length > 0) {
+                    this.form.submit()
+                }
+            })
+        }
+    }
+    catch { }
+}
