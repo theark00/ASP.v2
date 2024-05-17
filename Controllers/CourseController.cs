bdc71c35fb82de4962db05aa9aa57d6bc1b20d9f
theark00 <143406995+theark00@users.noreply.github.com>
2024-05-17 18:53:42 +0200

    Update CourseController.cs

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class CourseController : Controller
+[Authorize]
+public class CoursesController(HttpClient httpClient) : Controller
 {
-    public IActionResult Index()
+    private readonly HttpClient _httpClient = httpClient;
+    [Route("/courses")]
+    public async  Task<IActionResult> Index()
     {
-        return View();
+        var viewModel = new CourseIndexViewModel();
+
+
+        var response = await _httpClient.GetAsync("https://localhost:7229/api/courses");
+        if (response.IsSuccessStatusCode)
+        {
+            var courses = JsonConvert.DeserializeObject<IEnumerable<CourseViewModel>>(await response.Content.ReadAsStringAsync());
+            if (courses != null && courses.Any())
+                viewModel.Courses = courses;
+           
+        }
+
+        return View(viewModel);
     }
 }
